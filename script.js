017493087e747808a1059d98705f49662b74fbf8
robinwatts96 <robinwatts96@gmail.com>
2023-03-30 08:48:17 +0100

    Kind of works?

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ config()
 import { Configuration, OpenAIApi} from "openai"
 import readline from "readline"
 
-const angry = "Respond to all of the folloing prompts as if you are extremely grumpy and angry: "
+// const angry = "Respond to all of the following prompts as if you are extremely grumpy and angry: "
+const angry = "Read me a 10 word poem "
+
 
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.API_KEY
 }))
+
+
 
 const userInterface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
+
+openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: angry}],
+})
+.then(res => {
+    console.log(res.data.choices[0].message.content)
+})
 
 userInterface.prompt()
 userInterface.on("line", async input => {
@@ -25,11 +37,3 @@ userInterface.on("line", async input => {
     console.log(res.data.choices[0].message.content)
     userInterface.prompt()
 })
-
-// openai.createChatCompletion({
-//     model: "gpt-3.5-turbo",
-//     messages: [{role: "user", content: "respond as an angry person: hello"}],
-// })
-// .then(res => {
-//     console.log(res.data.choices[0].message.content)
-// })
